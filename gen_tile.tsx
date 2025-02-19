a8ec1152bc8430e205bc04e678aa2439731f8bef
yannis-messadia <yannis.messadia@laplateforme.io>
2025-02-19 16:07:39 +0100

    test map

---
+++ gen_tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gen_tile" tilewidth="32" tileheight="32" tilecount="1064" columns="4">
+ <image source="src/overworld/assets/map/tileset/gen_tile.png" width="128" height="8512"/>
+</tileset>
